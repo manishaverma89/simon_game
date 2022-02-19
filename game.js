ba5e38bc18e4afb57b0f9ee24bdd30f20a4ec6ad
manisha <manisha.verma23@gmail.com>
2022-02-19 07:10:54 +0530

    checking which buttonis pressed

--- game.js
+++ game.js
@@ -1,18 +1,4 @@
- // step-1 create a new function called nextSequence()
-
-// step-2 Generate a new random number between 0 and 3, and store it in a variable called randomNumber
-
-// step-3 create a new array called buttonColours and set it to hold the sequence "red", "blue", "green", "yellow"var
-
-// step-4 Create a new variable called randomChosenColour and use the randomNumber from step 2 to select a random colour from the buttonColours array.
-
-// step-5 create a new empty array called gamePattern
-
-// step-6 Add the new randomChosenColour generated in step 4 to the end of the gamePattern.
-// step-7 Use jQuery to select the button with the same id as the randomChosenColour
-// step-8 use jQuery to animate a flash to the button selected in step 7.
-// step-9 figure out how you can use Javascript to play the sound for the button colour selected in step 6.
-    
+     // Creating a new pattern
 
 var userClickedPattern = [];
 
@@ -26,6 +12,8 @@ function nextSequence(){                                           //step1
     // console.log(randomChosenColour);          check random colors displayed in console by calling nextSequence();
     gamePattern.push(randomChosenColour);                          //step6
     
+    // Show the Sequence to the User with Animations and Sounds
+
     var buttonId = ('#' + randomChosenColour)  ;                   //step7
      $(buttonId).fadeIn(200).fadeOut(200).fadeIn(200);             //step8
 
@@ -39,22 +27,19 @@ function nextSequence(){                                           //step1
 
 
 
-// chack which button is pressed
+        // checking which button is pressed
 
-// step-10  Use jQuery to detect when any of the buttons are clicked and trigger a handler function.
-    $(".btn").click(function(){
 
-// step-11 Inside the handler, create a new variable called userChosenColour to store the id of the button that got clicked.
-       
-      var userChosenColour =  $(this).attr("id");
+            $(".btn").click(function(){                  //step10
+
+            var userChosenColour =  $(this).attr("id");   //step11
     
-        
+            userClickedPattern.push(userChosenColour);     //step12
 
-// step-12  At the top of this file, create a new empty array with the name userClickedPattern.
+            //   console.log(userClickedPattern);
 
-//  step-13 Add the contents of the variable userChosenColour created in step 11 to the end of this new userClickedPattern 
-          userClickedPattern.push(userChosenColour);
-          console.log(userClickedPattern);
+            
+
 
     });
    
