770a95f5fb3cd60a4132189d24727b4390bf7067
manisha <manisha.verma23@gmail.com>
2022-02-19 16:34:33 +0530

    Added Animation to user clicks

--- game.js
+++ game.js
@@ -1,46 +1,65 @@
      // Creating a new pattern
 
 var userClickedPattern = [];
-
 var gamePattern = [];                                              //step5
+                                         
 buttonColours = ["red" ,"blue", "green","yellow"];                 //step3
 
-function nextSequence(){                                           //step1
+    function nextSequence(){                                           //step1
 
-    var randomNumber = Math.floor(Math.random() * 4 );             //step2
-    var randomChosenColour = buttonColours[randomNumber];          //step4  
-    // console.log(randomChosenColour);          check random colors displayed in console by calling nextSequence();
-    gamePattern.push(randomChosenColour);                          //step6
+         var randomNumber = Math.floor(Math.random() * 4 );             //step2
+         var randomChosenColour = buttonColours[randomNumber];          //step4  
+         // console.log(randomChosenColour);        check random colors displayed in console by calling nextSequence();
+        
+         gamePattern.push(randomChosenColour);                          //step6
     
-    // Show the Sequence to the User with Animations and Sounds
+         // Show the Sequence to the User with Animations and Sounds
 
-    var buttonId = ('#' + randomChosenColour)  ;                   //step7
-     $(buttonId).fadeIn(200).fadeOut(200).fadeIn(200);             //step8
-
-   
-        var buttonSound = ("sounds/"+ randomChosenColour +".mp3")  //step9
-        var audio = new Audio(buttonSound);
-        audio.play();
+         var buttonId = ('#' + randomChosenColour)  ;                   //step7
+         $(buttonId).fadeIn(200).fadeOut(200).fadeIn(200);             //step8
+         playSound(randomChosenColour);
+         animatePress(randomChosenColour);
+    
+        // var buttonSound = ("sounds/"+ randomChosenColour +".mp3")  //step9
+        // var audio = new Audio(buttonSound);
+        // audio.play();      //code added to function playSound()
         
     
-}
+         }
+    // checking which button is pressed
 
 
+            $(".btn").click(function(){                     //step10
 
-        // checking which button is pressed
-
-
-            $(".btn").click(function(){                  //step10
-
-            var userChosenColour =  $(this).attr("id");   //step11
+            var userChosenColour =  $(this).attr("id");     //step11
     
-            userClickedPattern.push(userChosenColour);     //step12
+            userClickedPattern.push(userChosenColour);      //step12
 
             //   console.log(userClickedPattern);
+            playSound(userChosenColour);
+            animatePress(userChosenColour);
 
             
-
-
-    });
+          });
    
+    // Add Sounds to Button Clicks
+           
 
+         function playSound(name){                                     //step15
+            var audio = new Audio("sounds/"+ name +".mp3");
+            audio.play();
+            
+         }
+
+        
+    // Add Animations To User Clicks
+         
+         function animatePress(currentColour){
+             $(".btn").addClass("pressed");
+             setTimeout(function() {
+                $(".btn").removeClass('pressed');
+            }, 100);
+
+         }
+
+         
